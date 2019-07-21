fd46bb5b038574514930d8d01725d63eebbedf3f
olHlo <tianlang427@sina.com>
2019-07-21 22:22:23 +0800

    update module dependency version

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/olHlo/go-tutorial
+module github.com/olHlo/demomodule
 
 go 1.12
+
+require github.com/olHlo/libmodule v1.0.1
---
+++ main.go
@@ -0,0 +1,10 @@
+package main;
+
+import (
+	"fmt"
+	"github.com/olHlo/libmodule"
+)
+
+func main()  {
+	fmt.Println(libmodule.Output("test"))
+}
